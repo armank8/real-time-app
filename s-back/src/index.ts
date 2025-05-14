8dd3f95866ef1b24ab1d6dbac7ad1ea9c693c61f
Arman-Kabir <armankabir8@gmail.com>
2025-05-15 05:39:02 +0600

    api routes

--- s-back/src/index.ts
+++ s-back/src/index.ts
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import Message from "./models/Message.js";
 import { timeStamp } from "console";
 await connectDB();
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -23,13 +24,17 @@ app.get("/", (req, res) => {
   res.send("Socket.IO server is up and running");
 });
 
+app.use("/api/auth", authRoutes);
+
+
+
 // socket io
 io.on("connection", async (socket) => {
   console.log("A user connected:", socket.id);
 
   // 1. Send previous messages
   const messages = await Message.find().sort({ timeStamp: 1 }).limit(50);
-  socket.emit("load-messages", messages);  // sends to newly connected clients
+  socket.emit("load-messages", messages); // sends to newly connected clients
 
   // 2. Receive new message
   socket.on("chat-message", async (msg) => {
@@ -49,5 +54,6 @@ io.on("connection", async (socket) => {
   });
 });
 
+// PORT & Listening
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ s-back/src/routes/auth.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const router = express.Router();
+const JWT_SECRET = "your-secret-key";
+
+// Register
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ message: "User exists" });
+
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await User.create({ name, email, password: hashed });
+  const token = jwt.sign(
+    { id: user._id, name: user.name, email: user.email },
+    JWT_SECRET
+  );
+  res.json({ token });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+  const token = jwt.sign(
+    { id: user._id, name: user.name, email: user.email },
+    JWT_SECRET
+  );
+  res.json({ token });
+});
+
+export default router;
